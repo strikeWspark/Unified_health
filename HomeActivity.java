1d13c891c36ad9c6ae9429cd1fcf2b041434bea0
strikeWspark <48147463+strikeWspark@users.noreply.github.com>
2019-11-17 23:10:11 +0530

    Add files via upload

--- HomeActivity.java
+++ HomeActivity.java
@@ -11,6 +11,7 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,6 +19,17 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSet;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.request.DataReadRequest;
+import com.google.android.gms.fitness.result.DataReadResponse;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -30,8 +42,13 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
 
+import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import static java.text.DateFormat.getDateInstance;
+import static java.text.DateFormat.getTimeInstance;
 
 
 public class HomeActivity extends AppCompatActivity {
@@ -91,6 +108,21 @@ public class HomeActivity extends AppCompatActivity {
 
         }*/
 
+
+        FitnessOptions fitnessOptions = FitnessOptions.builder()
+                .addDataType(DataType.TYPE_HEART_RATE_BPM, FitnessOptions.ACCESS_READ)
+                .build();
+
+        if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions)) {
+            GoogleSignIn.requestPermissions(
+                    this, // your activity
+                    997,
+                    GoogleSignIn.getLastSignedInAccount(this),
+                    fitnessOptions);
+        } else {
+            accessGoogleFit();
+        }
+
         emergencycall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -115,47 +147,47 @@ public class HomeActivity extends AppCompatActivity {
     }
 
 
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu){
-            getMenuInflater().inflate(R.menu.home_toolbar, menu);
-            return super.onCreateOptionsMenu(menu);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.uh_signout:
+                Toast.makeText(this, "Signout", Toast.LENGTH_SHORT).show();
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(HomeActivity.this, MainActivity.class));
+                finish();
         }
+        return super.onOptionsItemSelected(item);
+    }
 
-        @Override
-        public boolean onOptionsItemSelected (MenuItem item){
-            switch (item.getItemId()) {
-                case R.id.uh_signout:
-                    Toast.makeText(this, "Signout", Toast.LENGTH_SHORT).show();
-                    FirebaseAuth.getInstance().signOut();
-                    startActivity(new Intent(HomeActivity.this, MainActivity.class));
-                    finish();
+    @Override
+    protected void onStart() {
+        super.onStart();
+        homedialog.setMessage("Loading Details...");
+        homedialog.show();
+        homedialog.setCancelable(false);
+        mDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Uified uified = dataSnapshot.getValue(Uified.class);
+                name.setText(uified.getName());
+                heartrate.setText(uified.getHeartrate() + " BPM");
+                hemoglobin.setText(uified.getHaemoglobin() + " Hb");
+                bmi.setText(uified.getBMI());
+                homedialog.dismiss();
             }
-            return super.onOptionsItemSelected(item);
-        }
 
-        @Override
-        protected void onStart(){
-            super.onStart();
-            homedialog.setMessage("Loading Details...");
-            homedialog.show();
-            homedialog.setCancelable(false);
-            mDatabaseReference.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    Uified uified = dataSnapshot.getValue(Uified.class);
-                    name.setText(uified.getName());
-                    heartrate.setText(uified.getHeartrate() + " BPM");
-                    hemoglobin.setText(uified.getHaemoglobin() + " Hb");
-                    bmi.setText(uified.getBMI());
-                    homedialog.dismiss();
-                }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                }
-            });
+            }
+        });
         /*
         mDatabaseReference.addChildEventListener(new ChildEventListener() {
             @Override
@@ -197,6 +229,82 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
         */
-        }
-}
+    }
 
+    private void accessGoogleFit() {
+        Calendar cal = Calendar.getInstance();
+        Date now = new Date();
+        cal.setTime(now);
+        long endTime = cal.getTimeInMillis();
+        cal.add(Calendar.WEEK_OF_YEAR, -1);
+        long startTime = cal.getTimeInMillis();
+
+        java.text.DateFormat dateFormat = getDateInstance();
+        Log.i("100", "Range Start: " + dateFormat.format(startTime));
+        Log.i("100", "Range End: " + dateFormat.format(endTime));
+
+        DataReadRequest readRequest =
+                new DataReadRequest.Builder()
+                        // The data request can specify multiple data types to return, effectively
+                        // combining multiple data queries into one call.
+                        // In this example, it's very unlikely that the request is for several hundred
+                        // datapoints each consisting of a few steps and a timestamp.  The more likely
+                        // scenario is wanting to see how many steps were walked per day, for 7 days.
+                        .read(DataType.TYPE_HEART_RATE_BPM)
+                        // Analogous to a "Group By" in SQL, defines how data should be aggregated.
+                        // bucketByTime allows for a time span, whereas bucketBySession would allow
+                        // bucketing by "sessions", which would need to be defined in code.
+                        .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+                        .build();
+
+
+        Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this)).readData(readRequest)
+                .addOnSuccessListener(new OnSuccessListener<DataReadResponse>() {
+                    @Override
+                    public void onSuccess(DataReadResponse dataReadResponse) {
+                        DataSet dataSet = dataReadResponse.getDataSet(DataType.TYPE_HEART_RATE_BPM);
+                        dumpDataSet(dataSet);
+                    }
+                });
+
+    }
+
+
+    private void dumpDataSet(DataSet dataSet) {
+        Log.i("100", "Data returned for Data type: " + dataSet.getDataType().getName());
+        DateFormat dateFormat = getTimeInstance();
+
+        //me idhar mean nikal raha he, incase ek se jyada values aye
+        Integer mean=0;
+        int size=dataSet.getDataPoints().size();
+        for (DataPoint dp : dataSet.getDataPoints()) {
+            Log.i("111", "Data point:");
+            Log.i("111", "\tType: " + dp.getDataType().getName());
+            Log.i("111", "\tStart: " + dateFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)));
+            Log.i("111", "\tEnd: " + dateFormat.format(dp.getEndTime(TimeUnit.MILLISECONDS)));
+
+            for (Field field : dp.getDataType().getFields()) {
+                //yaha actual heartrate read ho raha he.
+                double d = Double.parseDouble(dp.getValue(field).toString());
+                int it=(int) d;
+                mean=mean+ it;
+                Log.i("111", "\tField: " + field.getName() + " Value: " + dp.getValue(field));
+            }
+        }
+        mean= mean/size;
+
+        mDatabaseReference.child("heartrate").setValue(mean.toString());
+        Log.e("500",mean.toString());
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == 997) {
+                accessGoogleFit();
+            }
+        }
+    }
+
+}
